06cc44bed112f2ba991cc41d982654901c8971f8
Fernando Rivas <frivas695@gmail.com>
2026-09-22 23:11:25 -0700

    Added hello and variables

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+# Python Basics: print 
+#
+print("Hello, Python!")
+
+#print displays a value.
---
+++ variables_types.py
@@ -0,0 +1,15 @@
+## Variables and Types 
+
+name = "Ada"        #str 
+
+age = 36            #int 
+
+temperature = 21.5  #float
+
+learning = True     #boolean
+
+print(name, age)
+
+print(type(temperature))
+
+
